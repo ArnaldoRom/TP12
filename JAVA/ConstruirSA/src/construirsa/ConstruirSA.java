9fdc3b0c2fbc6b6eb701af6ec3dae85be3fe86f5
ArnaldoRom <142050350+ArnaldoRom@users.noreply.github.com>
2023-10-05 21:41:56 -0300

    se realizo punto 6

--- JAVA/ConstruirSA/src/construirsa/ConstruirSA.java
+++ JAVA/ConstruirSA/src/construirsa/ConstruirSA.java
@@ -19,19 +19,21 @@ public class ConstruirSA {
         try {
             // TODO code application logic here
             
-            //1 Cargamos driver
+            //1- Cargamos driver
 
             Class.forName("org.mariadb.jdbc.Driver");
             
-            //2 conectamos
+            //2- Realizamos Conexion
             
             Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/construir sa","root","");
+            
+            //3- Insertar 3 empleados
             
 //            String sql="INSERT INTO empleado(dni,apellido,nombre,acceso,estado)VALUES(45182335,'Campo','Pedro',3,1)";
 //            String sq2="INSERT INTO empleado(dni,apellido,nombre,acceso,estado)VALUES(43121131,'Rocamora','Velentin',7,1)";
 //            String sq3="INSERT INTO empleado(dni,apellido,nombre,acceso,estado)VALUES(45182336,'Portal','juan',4,1)";
 //            
-//            //3 Cargamos datos
+//          
 //            PreparedStatement ps= conexion.prepareStatement(sql);
 //            PreparedStatement ps2= conexion.prepareStatement(sq2);
 //            PreparedStatement ps3= conexion.prepareStatement(sq3);
@@ -39,23 +41,32 @@ public class ConstruirSA {
 //            ps2.executeUpdate();
 //            ps3.executeUpdate();
             
+            //4- Insertar 2 Herramientas
             
 //            String sqlH="INSERT INTO herramienta(nombre,descripcion,stock,estado)VALUES('taladro','percutor',5,1),('Guantes','Anticorte',12,1)";
 //            
 //            PreparedStatement h=conexion.prepareStatement(sqlH);
 //            
 //            h.executeUpdate();
+
+            //5- Listar herramientas con stock mayor a 10
             
-            String lista="SELECT * FROM herramienta WHERE stock>10";
+//            String lista="SELECT * FROM herramienta WHERE stock>10";
+//            
+//            PreparedStatement her=conexion.prepareStatement(lista);           
+//            ResultSet res=her.executeQuery();
+//            
+//            while(res.next()){
+//                System.out.println("ID: "+ res.getInt("idHerramientas")+ " Nombre: "+res.getString("nombre")+" Descripcion: "+res.getString("descripcion")+" Stock: "+res.getInt("stock")+" Estado: "+res.getBoolean("estado"));
+//            }
             
-            PreparedStatement her=conexion.prepareStatement(lista);
+           //6- Dar de baja primer empleado
            
-            
-            ResultSet res=her.executeQuery();
-            
-            while(res.next()){
-                System.out.println("ID: "+ res.getInt("idHerramientas")+ " Nombre: "+res.getString("nombre")+" Descripcion: "+res.getString("descripcion")+" Stock: "+res.getInt("stock")+" Estado: "+res.getBoolean("estado"));
-            }
+           String sql="UPDATE empleado SET estado=0,acceso=0 WHERE idEmpleado=1";
+           
+           PreparedStatement baja=conexion.prepareStatement(sql);
+           baja.executeUpdate();
+           
             
             
             
